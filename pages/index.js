4d63443454a5df22462d82a664d01dee80c5bc8c
Mac12880 <marc.juan6.mj@gmail.com>
2023-11-02 18:38:25 +0800

    fix responsiveness

--- pages/index.js
+++ pages/index.js
@@ -113,7 +113,7 @@ export default function Home() {
                     </p>
                   </div>
                   <div className="linkButton">
-                    <button className="linkText">Visit Tagpros.org</button>
+                    <button className="linkText">Visit nrp.tagpros.us</button>
                   </div>
                 </div>
               </div>
@@ -123,6 +123,17 @@ export default function Home() {
           <section className="aboutUsContainer">
             <div className="d-flex flex-column align-items-center justify-content-center w-100 h-100">
               <div className="aboutUsBackground">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 1440 320"
+                  className="aboutUsWaveSVG"
+                >
+                  <path
+                    fill="#7BC0E8E8"
+                    fill-opacity="1"
+                    d="M0,96L80,122.7C160,149,320,203,480,197.3C640,192,800,128,960,117.3C1120,107,1280,149,1360,170.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+                  ></path>
+                </svg>
                 <div className="aboutUsWave">
                   <div className="aboutUsContent">
                     <div className="aboutUsTextVideo">
